cc6bd14561c48ef4f756fff6874bf59c7563c4bd
Max Gallant <mcgallant72@gmail.com>
2016-06-02 15:33:59 -0600

    Added CRD servies

--- app/controllers/listCtrl.js
+++ app/controllers/listCtrl.js
@@ -1,5 +1,5 @@
 angular.module("listaroo")
-  .controller("listCtrl", function($scope, listService) {
+  .controller("listCtrl", function($scope, $interval, listService) {
       $scope.message = "Hi";
       listService.getLists(function(response) {
           console.log(response.data);
@@ -7,14 +7,35 @@ angular.module("listaroo")
       });
 
       $scope.deleteList = function(list) {
-        listService.deleteList(list);
+        listService.deleteList(list, function(response) {
+          var id = response.data.id;
+          listIndex = findEntityById($scope.lists, id);
+          $scope.lists.splice(listIndex, 1);
+        });
       }
 
-      $scope.deleteListItem = function(listItem) {
-        listService.deleteListItem(listItem);
+      $scope.deleteListItem = function(list, listItem) {
+        listService.deleteListItem(listItem, function(response) {
+          var id = response.data.id;
+          listItemIndex = findEntityById(list.list_items, id);
+          list.list_items.splice(listItemIndex, 1);
+        });
       }
 
-      $scope.addListItem = function(list) {
-        listService.addListItem(list, $scope.newestListItem)
+      $scope.addListItem = function(list, content) {
+        listService.addListItem(list, content, function(response) {
+          list.list_items.push(response.data)
+        });
       }
+
+      function findEntityById(entityArray, id) {
+        for ( k = 0; k < entityArray.length; k++ ) {
+          if ( entityArray[k].id ===  id ) {
+            return k;
+          }
+        }
+        return null;
+      }
+
+
   });
--- app/services/listService.js
+++ app/services/listService.js
@@ -5,29 +5,33 @@ angular.module("listaroo")
         .then(callback);
     }
 
-    this.deleteList = function(list) {
+    this.deleteList = function(list, callback) {
       $http({
       method: 'DELETE',
       url: 'http://localhost:3000/api/lists/' + list.id
-      });
+      }).then(callback);
     }
 
-    this.deleteListItem = function(listItem) {
+    this.deleteListItem = function(listItem, callback) {
       $http.delete('http://localhost:3000/api/list_items/' + listItem.id)
+        .then(callback);
     }
 
-    this.addListItem = function(list, item_content) {
+    this.addListItem = function(list, item_content, callback) {
       $http({
         method: 'POST',
         url: 'http://localhost:3000/api/list_items',
         headers: {
-          'Content-Type' : 'text'
+          'Content-Type' : 'application/json'
         },
         data: {
-          'parent_list_id': list.id,
-          'content': item_content
+          'list_item' : {
+            'content' : item_content,
+            'list_id' : list.id
+          }
         }
-      });
+      })
+        .then(callback);
     }
 
   });
