c1a2bd8e910eebd8a6ca4d231a8759ea5915882b
Max Gallant <mcgallant72@gmail.com>
2016-06-10 09:00:01 -0700

    refactored url to remove magic strings

--- app/services/listService.js
+++ app/services/listService.js
@@ -1,14 +1,17 @@
 angular.module("listaroo")
+
   .service("listService", function($http) {
+    var rootUrl = "http://localhost:3000"
+
     this.getLists = function(callback) {
-      $http.get("http://localhost:3000/api/lists.json")
+      $http.get(rootUrl + "/api/lists.json")
         .then(callback);
     }
 
     this.addList = function(listTitle, callback) {
       $http({
         method: 'POST',
-        url: 'http://localhost:3000/api/lists',
+        url: rootUrl + '/api/lists',
         headers: {
           'Content-Type' : 'application/json'
         },
@@ -21,14 +24,14 @@ angular.module("listaroo")
     this.deleteList = function(list, callback) {
       $http({
       method: 'DELETE',
-      url: 'http://localhost:3000/api/lists/' + list.id
+      url: rootUrl + '/api/lists/' + list.id
       }).then(callback);
     }
 
     this.updateList = function(list, callback) {
       $http({
         method: 'PUT',
-        url: 'http://localhost:3000/api/lists/' + list.id,
+        url: rootUrl + '/api/lists/' + list.id,
         headers: {
           'Content-Type' : 'application/json'
         },
@@ -39,7 +42,7 @@ angular.module("listaroo")
     }
 
     this.deleteListItem = function(listItem, callback) {
-      $http.delete('http://localhost:3000/api/list_items/' + listItem.id)
+      $http.delete(rootUrl + '/api/list_items/' + listItem.id)
         .then(callback);
     }
 
