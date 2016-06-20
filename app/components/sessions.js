0029c4e1acd4fd6183d84d528369dd47aadb1daf
Max Gallant <mcgallant72@gmail.com>
2016-06-20 09:07:43 -0600

    added styles and started error handling

--- app/components/sessions.js
+++ app/components/sessions.js
@@ -3,11 +3,29 @@ angular.module("listaroo").
     templateUrl: '/views/login.html',
     controller: function($scope, $cookies, $location, sessionService) {
 
+      $scope.errorsPresent = false;
+      $scope.errorMessages = [];
+      $scope.usernameRequired = "You must provide a username to log in"
+      $scope.passwordRequired = "You must provide a password to log in"
+
       $scope.submitLogin = function(username, password) {
-        sessionService.submitLogin(username, password, function(response) {
-          $cookies.putObject('user', response.data);
-          $location.path('/teams');
-        })
+        $scope.errorsPresent = false;
+        username = username.trim();
+        password = password.trim();
+        if (username.length < 1 ) {
+          $scope.errorsPresent = true;
+          $scope.errorMessages.push($scope.usernameRequired);
+        }
+        if (password.length < 1 ) {
+          $scope.errorsPresent = true;
+          $scope.errorMessages.push($scope.passwordRequired);
+        }
+        if (!$scope.errorsPresent) {
+          sessionService.submitLogin(username, password, function(response) {
+            $cookies.putObject('user', response.data);
+            $location.path('/teams');
+          });
+        }
       }
 
     }
